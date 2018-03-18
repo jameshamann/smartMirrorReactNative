1303b3a9cf0739d773b3eaa5875fdde4559cd5d4
James Hamann <jameshamann@me.com>
2018-03-18 16:06:42 +0000

    working on authflow

--- App.js
+++ App.js
@@ -14,15 +14,20 @@ import {
 } from 'react-native';
 
 import { Drawer, Title, Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
-import SideBar from './screens/components/sideBar'
-import MainContent from './screens/components/mainContent'
+import SideBar from './screens/components/sidebar'
 import Login from './screens/login'
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
+import { Auth } from 'aws-amplify';
+
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
 
-export default class App extends Component {
+import { withAuthenticator } from 'aws-amplify-react-native';
+import { Authenticator } from 'aws-amplify-react-native'
+
+
+class App extends Component {
 
 
   closeDrawer = () => {
@@ -61,6 +66,19 @@ export default class App extends Component {
                     <Login />
                     </Drawer>
             </Container>
+
         );
       }
     }
+
+class AppWithAuth extends Component {
+  render() {
+    return (
+      <Authenticator>
+        <App />
+      </Authenticator>
+    );
+  }
+}
+
+export default AppWithAuth;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import AppWithAuth from './App';
 
-AppRegistry.registerComponent('smartMirrorReactNative', () => App);
+AppRegistry.registerComponent('smartMirrorReactNative', () => AppWithAuth);
