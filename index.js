29ad29238d8cdb0fe34c75401224afb944dc00a1
James Hamann <jameshamann@me.com>
2018-03-19 22:16:54 +0000

    setting up aws iot client

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { Buffer } from 'buffer'
+import process from 'process'
+
+global.Buffer = Buffer
+global.process = process
 
 AppRegistry.registerComponent('smartMirrorReactNative', () => App);
