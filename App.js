ebcfef4f982339ba06b08ef4981b5b5a6b7dcac0
James Hamann <jameshamann@me.com>
2018-03-22 20:59:42 +0000

    workong on getting curr user

--- App.js
+++ App.js
@@ -16,56 +16,27 @@ import {
 import { Drawer, Title, Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
 import SideBar from './screens/components/sidebar'
 import MainContent from './screens/components/mainContent'
+import { AmazonCognitoIdentity, CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
 
 import Login from './screens/login'
-import Amplify from 'aws-amplify';
-import { Auth } from 'aws-amplify';
-
+import Amplify from 'aws-amplify-react-native';
 import aws_exports from './aws-exports';
-
+import { withAuthenticator, API } from 'aws-amplify-react-native';
 Amplify.configure(aws_exports);
 
-import { withAuthenticator } from 'aws-amplify-react-native';
-import { Authenticator } from 'aws-amplify-react-native'
-
-import Aws from 'aws-sdk/dist/aws-sdk-react-native'
-import AwsIot from 'aws-iot-device-sdk'
 
 class App extends Component {
 
   componentDidMount(){
-    Aws.config.region = AWS_REGION
-    Aws.config.credentials = new Aws.CognitoIdentityCredentials({
-        IdentityPoolId: AWS_COGNITO_IDENTITY_POOL
-    })
 
-    Aws.config.credentials.get(() => {
-        const config = {}
-        let client
 
-        config.host = AWS_IOT_ENDPOINT
-        config.protocol = 'wss'
-        config.clientId = `client-${Math.floor((Math.random() * 100000) + 1)}`
-        config.accessKeyId = Aws.config.credentials.accessKeyId
-        config.secretKey = Aws.config.credentials.secretAccessKey
-        config.sessionToken = Aws.config.credentials.sessionToken
-
-        client = AwsIot.device(config)
-
-        client.on('connect', () => {
-            client.publish('topic_1')
-        })
-
-        client.on('message', (topic, message) => {
-            console.log(topic, message)
-        })
-
-        client.on('error', error => {
-            console.log(error)
-        })
-    })
+  var poolData = { UserPoolId : 'eu-west-2_WE3gHDxq6',
+        ClientId : '4tprh2pphe9gl93o4qh20scvtd'
+    };
+    var userPool = new CognitoUserPool(poolData);
+    var cognitoUser = userPool.getCurrentUser();
+    console.log(cognitoUser)
   }
-
 
   closeDrawer = () => {
         this._drawer._root.close();
